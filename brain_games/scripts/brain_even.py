7acaf1b3eab28e8db29b81e008abd7d15b2a27d0
Chadina <dasha_dasha_dash@mail.ru>
2023-04-14 17:36:47 +0700

    myproject correction

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 
-from brain_games.games.even_game import main
+from brain_games.games.even_game import main as func_game
+
+
+def main():
+    func_game()
+
 
 if __name__ == '__main__':
     main()
